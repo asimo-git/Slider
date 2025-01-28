44bf70a22e74113d4ca6d8dd8104b7ec732aae71
Asimo <rvolenc@gmail.com>
2025-01-28 11:36:49 +0400

    feat: add animation to pagination buttons

--- app/components/slider/SliderPagination.tsx
+++ app/components/slider/SliderPagination.tsx
@@ -20,30 +20,18 @@ export default function SliderPagination({
 
   return (
     <div aria-label="Slider pagination" className="flex gap-2 mb-5">
-      {Array.from({ length: numberOfPaginationButtons }, (_, index) =>
-        index === activeSlide ? (
-          <svg
-            width="40"
-            height="8"
-            viewBox="0 0 40 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            key={index}
-          >
-            <rect width="40" height="8" rx="4" fill="#CCCCCC" />
-            <rect width="20" height="8" rx="4" fill="#1C1C1C" />
-          </svg>
-        ) : (
-          <button
-            key={index}
-            onClick={() => handleSlideChange(index)}
-            className={`rounded-full w-2 h-2 transition-transform duration-200 ${
-              index === activeSlide ? "bg-black w-10" : "bg-gray-300"
-            }`}
-            aria-label={`Go to slide ${index + 1}`}
-          ></button>
-        )
-      )}
+      {Array.from({ length: numberOfPaginationButtons }, (_, index) => (
+        <button
+          key={index}
+          onClick={() => handleSlideChange(index)}
+          className={`rounded-full h-2 transition-all duration-500 ease-in-out ${
+            index === activeSlide
+              ? "bg-buttonBg pointer-events-none w-10 before:content-[''] before:w-5 before:h-2 before:bg-black before:rounded-full before:absolute before:left-0 before:top-0 transition-all duration-500 ease-in-out relative"
+              : "bg-buttonBg w-2"
+          }`}
+          aria-label={`Go to slide ${index + 1}`}
+        ></button>
+      ))}
     </div>
   );
 }
