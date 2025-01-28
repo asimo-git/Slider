5281f2d60bef445f3818402f0075c85f7f36b09e
Asimo <rvolenc@gmail.com>
2025-01-28 12:18:26 +0400

    fix: change header padding

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <header className="flex flex-wrap mb-10 text-[26px] leading-[28px]  md:text-[40px] md:leading-[44px] text-accent pb-3 md:border-b border-accent">
-        <div className="pr-[386px] border-b border-accent md:border-0 pb-2 mb-2 w-full md:w-min">
+        <div className="sm:pr-[386px] border-b border-accent md:border-0 pb-2 mb-2 w-full md:w-min">
           1.0
         </div>
         <h1>Наши услуги</h1>
